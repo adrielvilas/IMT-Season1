d7b6b4f9151806dca8a583f22c066203024bbbec
Retr0 <90638499+Eikebarbosa@users.noreply.github.com>
2024-05-09 10:47:15 -0300

    Update Player.java

--- Projeto-Integrador/Player.java
+++ Projeto-Integrador/Player.java
@@ -35,14 +35,14 @@ public class Player extends Entity {
     }
     public void getPlayerImage(){
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/images/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/images/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/images/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/images/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/images/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/images/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/images/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/images/boy_right_2.png"));
             
         
         }catch(IOException e){
@@ -50,7 +50,10 @@ public class Player extends Entity {
         }
     }
     public void update(){
-        if(keyH.upPressed == true){
+        if (keyH.upPressed == true || keyH.downPressed == true || 
+                keyH.leftPressed == true || keyH.rightPressed == true){
+            
+            if(keyH.upPressed == true){
             direction = "up";
             y = y - speed;
         }
@@ -66,27 +69,63 @@ public class Player extends Entity {
             direction = "right";
             x += speed;
         }
+        
+        spriteCounter++;
+        if(spriteCounter > 10){
+            if(spriteNum == 1){
+                spriteNum = 2;
+            }
+            else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+        }
+        
+        
     }
     public void draw(Graphics2D g2){
         
         BufferedImage image = null;
         
-        switch(direction){
+            switch(direction){
             case "up":
-                image = up1;
+                if(spriteNum == 1){
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                
+                }
                 break;
             case "down":
-                image = down1;
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                
                 break;
            
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);//37,37
         
         
     }
